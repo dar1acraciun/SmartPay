efc159a2c764bcc52756d86fba28e9a8f4f6f2a6
dar1a-admin <daria.craciun@endava.com>
2025-09-08 13:14:51 +0300

    new folders

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-from controller import routes  # presupunem că ai un fișier routes.py în controller/
+from controller import routes
+from fastapi.middleware.cors import CORSMiddleware  # presupunem că ai un fișier routes.py în controller/
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Înregistrează rutele definite în controller/routes.py
 app.include_router(routes.router)
