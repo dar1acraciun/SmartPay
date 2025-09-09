53de56f1c841fedf7a60d098c2017c37447d71f8
AdelinaPatlatii <adelina.patlatii@gmail.com>
2025-09-09 18:41:59 +0300

    ceva s-a intamplat aici

--- controller/report_controller.py
+++ controller/report_controller.py
@@ -1,5 +1,8 @@
 import os
 import json
+from pathlib import Path
+from model.file_model import File
+from db import SessionLocal
 from fastapi import HTTPException
 from hackathon_mastercard_regressor.evaluate_model import generate_shap_explanations
 from model.report_model import Report
@@ -28,14 +31,27 @@ async def generate_report_controller(source_id: str) -> str:
     if not source_id:
         raise HTTPException(status_code=400, detail="Missing source_id")
 
+
+    BASE_DIR = Path(__file__).resolve().parent
     session = SessionLocal()
+    file = File.get_file(session, source_id)
     try:
-        base_path = os.path.join(os.path.dirname(__file__), '..', 'hackathon_mastercard_regressor')
-        generate_shap_explanations(
-            model_path=os.path.join(base_path, "xgb_model_interchange_fee_rate.pkl"),
-            x_test_path=os.path.join(base_path, "x_test.csv"),
-            full_txn_path=os.path.join(base_path, "mastercard_transactions.csv")
-        )
+        if file.brand == "mastercard":
+            pkg_dir = (BASE_DIR / ".." / "hackathon_mastercard_regressor").resolve()
+            from hackathon_mastercard_regressor.evaluate_model import generate_shap_explanations
+            generate_shap_explanations(
+                model_path=os.path.join(pkg_dir, "xgb_model_interchange_fee_rate.pkl"),
+                x_test_path=os.path.join(pkg_dir, "x_test.csv"),
+                full_txn_path=os.path.join(pkg_dir, "mastercard_transactions.csv")
+            )
+        elif file.brand == "visa":
+            pkg_dir = (BASE_DIR / ".." / "hackathon_visa_regressor").resolve()
+            from hackathon_visa_regressor.evaluate_model import generate_shap_explanations
+            generate_shap_explanations(
+                model_path=os.path.join(pkg_dir, "xgb_model_interchange_fee_rate.pkl"),
+                x_test_path=os.path.join(pkg_dir, "x_test.csv"),
+                full_txn_path=os.path.join(pkg_dir, "visa_transactions.csv")
+            )
 
         report = Report(source_file=source_id, timestamp=datetime.datetime.now(datetime.timezone.utc))
         report.insert_report(session, brand="mastercard")
