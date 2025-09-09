70c2e01d4a7bde630f7960b0fd2804d8e539af38
Miruna <curduman.miruna@gmail.com>
2025-09-09 22:05:36 +0300

    haleluia reports generated for mastercard

--- controller/report_controller.py
+++ controller/report_controller.py
@@ -3,9 +3,36 @@ import json
 from fastapi import HTTPException
 from hackathon_mastercard_regressor.evaluate_model import generate_shap_explanations
 from model.report_model import Report
+from model.file_model import File
 import datetime, uuid
 from db import SessionLocal
+import pandas as pd
 import json
+
+FEATURES_MASTERCARD = [
+    "mc_pos_entry_mode",
+    "mc_eci_indicator",
+    "mc_ucaf_collection_indicator",
+    "mc_cvv2_result_code",
+    "mc_avs_result_code",
+    "mc_cross_border_indicator",
+    "mcc_group",
+    "channel_type",
+]
+
+FEATURES_VISA = [
+    "mc_pos_entry_mode",
+    "mc_eci_indicator",
+    "mc_ucaf_collection_indicator",
+    "mc_cvv2_result_code",
+    "mc_avs_result_code",
+    "mc_cross_border_indicator",
+    "mcc_group",
+    "channel_type",
+]
+    
+
+UPLOAD_DIR = "reports"
 
 
 async def get_report_controller(report_id: str):
@@ -23,47 +50,62 @@ async def get_report_controller(report_id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error reading report: {str(e)}")
 
+def save_report_json(report_json, report_id):
+    reports_dir = os.path.join(os.path.dirname(__file__), '..', 'reports')
+    os.makedirs(reports_dir, exist_ok=True)
+    report_path = os.path.join(reports_dir, f"{report_id}.json")
+    with open(report_path, "w", encoding="utf-8") as f:
+        json.dump(report_json, f, ensure_ascii=False, indent=2)
 
 async def generate_report_controller(source_id: str) -> str:
+    
     if not source_id:
         raise HTTPException(status_code=400, detail="Missing source_id")
+    file = File.get_file(SessionLocal(), source_id)
+    if not file:
+        raise HTTPException(status_code=404, detail="Source file not found in database")
+    
 
     # Caută fișierul CSV în folderul files
-    csv_path = os.path.join(os.path.dirname(__file__), '..', 'files', f'{source_id}.csv')
+    csv_path = os.path.join(os.path.dirname(__file__), '..', 'files', f'{file.id}.csv')
     csv_path = os.path.abspath(csv_path)
+    file_source = pd.read_csv(csv_path)
+    file_only_features = None
     if not os.path.exists(csv_path):
         raise HTTPException(status_code=404, detail="Source CSV file not found")
 
-    try:
-        import shutil
+    report_json = {}
+    if file.brand.lower() == "mastercard":
+        file_only_features = file_source[FEATURES_MASTERCARD]
         base_path = os.path.join(os.path.dirname(__file__), '..', 'hackathon_mastercard_regressor')
-        x_test_path = os.path.join(base_path, "x_test.csv")
-        # Copiază fișierul CSV sursă ca x_test.csv
-        shutil.copyfile(csv_path, x_test_path)
-
-        generate_shap_explanations(
+        report_json = generate_shap_explanations(
             model_path=os.path.join(base_path, "xgb_model_interchange_fee_rate.pkl"),
-            x_test_path=x_test_path,
-            full_txn_path=csv_path
+            file_only_features=file_only_features,
+            file_source=file_source
         )
-
-        # Citește fișierul JSON generat (presupunem că se numește shap_per_transaction_only.json)
-        json_path = os.path.join(base_path, "shap_per_transaction_only.json")
-        if not os.path.exists(json_path):
-            return {"error": "JSON report file not found after generation."}
-        with open(json_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
-
-        # Salvează conținutul JSON în folderul reports cu numele <source_id>.json
-        reports_dir = os.path.join(os.path.dirname(__file__), '..', 'reports')
-        os.makedirs(reports_dir, exist_ok=True)
-        report_file_path = os.path.join(reports_dir, f"{source_id}.json")
-        with open(report_file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-
-        return {"success": True, "message": "Report JSON saved.", "file": f"{source_id}.json"}
-    except Exception as e:
-        return {"error": str(e)}
+    elif file.brand.lower() == "visa":
+        file_only_features = file_source[FEATURES_VISA]
+        base_path = os.path.join(os.path.dirname(__file__), '..', 'hackathon_mastercard_regressor')
+        report_json = generate_shap_explanations(
+            model_path=os.path.join(base_path, "xgb_model_interchange_fee_rate.pkl"),
+            file_only_features=file_only_features,
+            file_source=file_source
+        )
+    
+    with SessionLocal() as session:
+        report = Report(
+            source_file=source_id,
+            timestamp=datetime.datetime.utcnow(),
+            brand=file.brand
+        )
+        report.insert_report(session, file.brand)
+        report.path = f"/reports/{report.id}.json"
+        session.commit()
+        save_report_json(report_json, report.id)
+        return {"message": "Report JSON saved.", "report_id": report.id, "path": report.path}
+    
+	#Error if not returned
+    return HTTPException(status_code=500, detail="Failed to generate report")
 
 
 async def get_all_reports_controller():
